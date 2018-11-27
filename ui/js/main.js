d900935f1a75e381b45ef43647ac5837aff0c33b
NBens <benshaqi.nizar@gmail.com>
2018-11-27 18:56:45 +0000

    JavaScript

---
+++ ui/js/main.js
@@ -0,0 +1,6 @@
+function showThenHide(div1, div2) {
+    var download = document.getElementById(div1);
+    download.style.display="block";
+    var send = document.getElementById(div2);
+    send.style.display="none";    
+}
